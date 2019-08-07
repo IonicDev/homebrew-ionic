3255cd5dbae6d005c755b6d597cc3da7ea297088
apatterson <apatterson@ionicsecurity.com>
2019-08-07 15:37:05 -0700

    update ionic-profiles stanza

--- Casks/ionic-profiles.rb
+++ Casks/ionic-profiles.rb
@@ -1,9 +1,9 @@
 cask 'ionic-profiles' do
-  version '2.2.0-307'
-  sha256 'b7e38ba243e6330c1e833d953f4f2c88579fd8136f75b6d688aff29640fb8dfc'
+  version '2.3.0-587'
+  sha256 '9cc52c91243996d3acb563796a37505d597b4ddaeb8ead54af90ab42e2254a36'
 
   # github.com/IonicDev/ionic-profiles/releases/download/2.2.0-307/ionic-profiles_macOS.zip was verified as official when first introduced to the cask
-  url 'https://github.com/IonicDev/ionic-profiles/releases/download/2.2.0-307/ionic-profiles_macOS.zip'
+  url 'https://github.com/IonicDev/ionic-profiles/releases/download/2.3.0-587/ionic-profiles_macOS.zip'
   appcast 'https://github.com/IonicDev/ionic-profiles/releases.atom'
   name 'ionic-profiles'
   homepage 'https://dev.ionic.com/tools/ionic-profiles'
