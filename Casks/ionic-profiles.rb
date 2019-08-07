4440e5da5a5e7ea7ca1ad3a4c627e20c4de326e5
apatterson <apatterson@ionicsecurity.com>
2019-08-07 15:56:57 -0700

    use zip checksum

--- Casks/ionic-profiles.rb
+++ Casks/ionic-profiles.rb
@@ -1,6 +1,6 @@
 cask 'ionic-profiles' do
   version '2.3.0-587'
-  sha256 '9cc52c91243996d3acb563796a37505d597b4ddaeb8ead54af90ab42e2254a36'
+  sha256 '16177e2595ac778eabc6a5038ebe9ab53b864fea4496db68afb4ae718dff8b04'
 
   # github.com/IonicDev/ionic-profiles/releases/download/2.2.0-307/ionic-profiles_macOS.zip was verified as official when first introduced to the cask
   url 'https://github.com/IonicDev/ionic-profiles/releases/download/2.3.0-587/ionic-profiles_macOS.zip'
