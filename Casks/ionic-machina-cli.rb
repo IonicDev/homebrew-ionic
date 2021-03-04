92b74171ec41705671f6adfc757be60d55158bff
Michael Floyd <mfloyd@is-mbp-mfloyd.in.ionicsecurity.com>
2021-03-04 11:53:06 -0800

    Update MacOS package

--- Casks/ionic-machina-cli.rb
+++ Casks/ionic-machina-cli.rb
@@ -1,12 +1,11 @@
 cask 'ionic-machina-cli' do
   version '2.1.0-8'
-  sha256 "9e08cc5de0b038a0dad3f3183c76cd60f54ab2068ed098b3e5d83392a7c3eec4
-"
+  sha256 "9e08cc5de0b038a0dad3f3183c76cd60f54ab2068ed098b3e5d83392a7c3eec4"
 
   # github.com/IonicDev/ionic-machina-cli/releases/download/1.8.1/MachinaTools-CLIforMac-1.8.1.tar.gz
   # was verified as official when first introduced to the cask
   # url to be verified (sha value already calculated)
-  url "https://github.com/IonicDev/ionic-machina-cli/releases/download/v2.1.0.8/MachinaTools-CLIforMac-2.1.0.tar.gz"
+  url "https://github.com/IonicDev/ionic-machina-cli/releases/download/v2.1.0.8/MachinaTools-CLImacOS-2.1.0.gz"
   appcast 'https://github.com/IonicDev/ionic-machina-cli/releases.atom'
   name "machina-cli"
   homepage "https://dev.ionic.com/tools/machina"
